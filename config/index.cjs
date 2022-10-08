d48aee6c4ee61596deb1181d841849596b505525
Sciurus-vulgaris <115361479+Sciurus-vulgaris@users.noreply.github.com>
2022-10-09 02:53:36 +1100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,35 +23,29 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ow_SK5lpRQj_inrEwYqSyC94QR5o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7Bob4ZGPQWtXgjRcmSPYHv-iK2eaQAZc3JNDGOQCuVA',
+      useTemplateId: 'TnCzU9yqs5_Zh2HXS68uz_UUwaVPWD2_aknPCKxM2iw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-10',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '啤啤', year: '2000', date: '10-10',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2021', date: '01-30',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2021-02-26' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '7Bob4ZGPQWtXgjRcmSPYHv-iK2eaQAZc3JNDGOQCuVA',
+  CALLBACK_TEMPLATE_ID: 'TnCzU9yqs5_Zh2HXS68uz_UUwaVPWD2_aknPCKxM2iw',
 
   CALLBACK_USERS: [
     {
