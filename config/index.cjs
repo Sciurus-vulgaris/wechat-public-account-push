1cfb61d09c4987270ad83992a2c9364cfcc768cd
Sciurus-vulgaris <115361479+Sciurus-vulgaris@users.noreply.github.com>
2022-10-09 17:43:37 +1100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -37,7 +37,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '啤先生',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ow_SK5lpRQj_inrEwYqSyC94QR5o',
+      id: 'ow_SK5g4riurIZDmJJM0qIWFHU-k',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'TnCzU9yqs5_Zh2HXS68uz_UUwaVPWD2_aknPCKxM2iw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
